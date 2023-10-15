840e9082274f10a0012183bd301dbf93041070c8
MaSch <Mathias.scheltjens@gmail.com>
2023-10-15 14:42:43 +0200

    inputs fixed

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -8,7 +8,7 @@ export const SubmitButton = () => {
   return (
     <button
       type='submit'
-      className='hover: diabaled:bg-opacity-65 group inline-flex w-fit items-center gap-2 rounded-full bg-gray-900 px-4 py-2 capitalize text-white hover:scale-110 hover:cursor-pointer hover:bg-gray-800 focus:scale-110 active:scale-105 disabled:scale-100'
+      className='hover:disabled:bg-opacity-65 dark:hover:text-white-/80dark:active:scale-105 group inline-flex w-fit items-center gap-2 rounded-full bg-gray-900 px-4 py-2 capitalize text-white transition-all hover:scale-110 hover:cursor-pointer hover:bg-gray-800 focus:scale-110 active:scale-105 disabled:scale-100 dark:border dark:border-gray-800 dark:hover:scale-110  dark:hover:cursor-pointer dark:hover:bg-gray-800 dark:disabled:scale-100'
     >
       {pending ? (
         <div className='full h-5 w-5 animate-spin rounded-full border-b-2  border-white'></div>

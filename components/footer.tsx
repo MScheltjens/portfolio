567fffa339637f7082f8cfc32e94f8c650838eb2
MaSch <Mathias.scheltjens@gmail.com>
2023-10-15 14:04:13 +0200

    simple footer added

---
+++ components/footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <footer className='mx-auto p-4 text-center text-xs opacity-50'>
+      <small className='mb-2 block text-xs'>2023 Mathias Scheltjens. All righs reserved.</small>
+      <p>Built with React, Next.js (App Directory & server-a ctions), Framer-Motion, Resend and TailwindCSS</p>
+    </footer>
+  );
+};

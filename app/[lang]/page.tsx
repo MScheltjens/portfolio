96ab28dca75f52f6a3da49b1d9bf38254329076c
Mathias S <Mathias.scheltjens@gmail.com>
2024-10-03 15:20:10 +0200

    basic i18n routing setup

---
+++ app/[lang]/page.tsx
@@ -0,0 +1,13 @@
+import { getDictionary } from '../../get-dictionary';
+import { Locale } from '../../i18n-config';
+
+export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
+  const dictionary = await getDictionary(lang);
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <main>
+        <h1>Mathias S. | {dictionary['web-developer'].title}</h1>
+      </main>
+    </div>
+  );
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <main>Mathias S. | Web Developer</main>
-    </div>
-  );
-}

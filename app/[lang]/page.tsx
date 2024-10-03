4ae6be3552bc91a98e68ad19b2bd75a5517e1a63
Mathias S <Mathias.scheltjens@gmail.com>
2024-10-03 16:07:02 +0200

    shadcn theme integration

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,11 +1,10 @@
 import { getDictionary } from '../../get-dictionary';
 import { Locale } from '../../i18n-config';
-import LocaleSwitcher from '../components/locale-switcher';
 
 export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
   const dictionary = await getDictionary(lang);
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="w-screen h-screen flex justify-center items-center container mx-auto">
       <main>
         <h1>Mathias S. | {dictionary['web-developer'].title}</h1>
       </main>

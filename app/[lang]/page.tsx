7378131b178431443733b6fca477e7424c509dcb
Mathias S <Mathias.scheltjens@gmail.com>
2024-10-03 15:47:11 +0200

    basic locale selection component

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,5 +1,6 @@
 import { getDictionary } from '../../get-dictionary';
 import { Locale } from '../../i18n-config';
+import LocaleSwitcher from '../components/locale-switcher';
 
 export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
   const dictionary = await getDictionary(lang);
@@ -7,6 +8,7 @@ export default async function Home({ params: { lang } }: { params: { lang: Local
     <div className="w-screen h-screen flex justify-center items-center">
       <main>
         <h1>Mathias S. | {dictionary['web-developer'].title}</h1>
+        <LocaleSwitcher />
       </main>
     </div>
   );

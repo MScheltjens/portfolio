28de6e5cfde82db05b89c904adb811c088234711
Mathias S <Mathias.scheltjens@gmail.com>
2024-10-04 13:45:58 +0200

    fix dictionaries import

--- i18/get-dictionary.ts
+++ i18/get-dictionary.ts
@@ -4,8 +4,8 @@ import type { Locale } from './config';
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
 const dictionaries = {
-  en: () => import('../dictionaries/en.json').then((module) => module.default),
-  de: () => import('../dictionaries/de.json').then((module) => module.default),
+  en: () => import('@/i18/dictionaries/en.json').then((module) => module.default),
+  de: () => import('@/i18/dictionaries/de.json').then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]?.() ?? dictionaries.en();

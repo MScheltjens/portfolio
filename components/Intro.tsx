c5152aac5db6a517d533c52abafc51dc2fdcb73f
Mathias S <Mathias.scheltjens@gmail.com>
2024-10-09 08:04:16 +0200

    style tweaks

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,13 +7,9 @@ type Props = {
   description: string;
 };
 
-export const Intro = async ({
-  profession1,
-  profession2,
-  description
-}: Props) => {
+export const Intro = ({ profession1, profession2, description }: Props) => {
   return (
-    <div className="mx-auto mt-24 flex flex-col-reverse items-center gap-x-10 gap-y-4 md:flex-row">
+    <div className="mx-auto mt-36 flex flex-col-reverse items-center gap-x-10 gap-y-4 md:flex-row">
       <div className="flex flex-1 flex-col">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">
           Mathias S<span className="text-accent">.</span>

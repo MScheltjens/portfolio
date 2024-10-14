0b60332563d45b265ce908074fcd1130ba34602b
Mathias S <Mathias.scheltjens@gmail.com>
2024-10-14 12:27:03 +0200

    nl and fr added

--- i18/get-dictionary.ts
+++ i18/get-dictionary.ts
@@ -5,6 +5,8 @@ import type { Locale } from './config';
 // We also get the default import for cleaner types
 const dictionaries = {
   en: () => import('./dictionaries/en.json').then((module) => module.default),
+  nl: () => import('./dictionaries/nl.json').then((module) => module.default),
+  fr: () => import('./dictionaries/fr.json').then((module) => module.default),
   de: () => import('./dictionaries/de.json').then((module) => module.default)
 };
 
